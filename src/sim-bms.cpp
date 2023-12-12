203e817f34818c338f26820de87a39e137d938f2
Lang, Samuel <slang@bethel.jw.org>
2023-12-12 18:59:31 +0100

    lala

--- src/sim-bms.cpp
+++ src/sim-bms.cpp
@@ -27,7 +27,7 @@
 #include <BLE2902.h>
 
 BLEServer *pServer = NULL;
-// BLECharacteristic *pTxCharacteristic = NULL;
+BLECharacteristic *pTxCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 class MyServerCallbacks : public BLEServerCallbacks
@@ -44,15 +44,36 @@ class MyServerCallbacks : public BLEServerCallbacks
     deviceConnected = false;
   }
 };
+// 0000ff01-0000-1000-8000-00805f9b34fb
+// NOTIFY, READ
+// Notifications from this characteristic is received data from BMS
 
-uint8_t basicInfoRequest[] = {0xDD, 0xA5, 0x03, 0x00, 0xFF, 0xFD, 0x77};
-uint8_t cellInfoRequest[] = {0xDD, 0xA5, 0x04, 0x00, 0xFF, 0xFC, 0x77};
-uint8_t deviceInfoRequest[] = {0xDD, 0xA5, 0x05, 0x00, 0xFF, 0xFB, 0x77};
+// 0000ff02-0000-1000-8000-00805f9b34fb
+// Write this characteristic to send data to BMS
+// READ, WRITE, WRITE NO RESPONSE
+static BLEUUID serviceUUID("0000ff00-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
+static BLEUUID charUUID_tx("0000ff01-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
+static BLEUUID charUUID_rx("0000ff02-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
+
+static BLEUUID faServiceUUID("0000fa01-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
+
+BLECharacteristic *m_pnpCharacteristic; // 0x2a50
 
 class MyRXCallbacks : public BLECharacteristicCallbacks
 {
   void onWrite(BLECharacteristic *pCharacteristic)
   {
+
+uint8_t basicInfoRequest[] = {0xDD, 0xA5, 0x03, 0x00, 0xFF, 0xFD, 0x77};
+uint8_t cellInfoRequest[] = {0xDD, 0xA5, 0x04, 0x00, 0xFF, 0xFC, 0x77};
+uint8_t deviceInfoRequest[] = {0xDD, 0xA5, 0x05, 0x00, 0xFF, 0xFB, 0x77};
+uint8_t testRequest[] = {0x11};
+    // {DD A5 00 1B 13 78 00 00 00 00 03 E8 00 00 22 C7 00 00 00 00 00 00 19 00 03 0C 02 0B 64 0B 5F FC 83 77};
+    uint8_t basicInfoResponse[] = {0xDD, 0xA5, 0x00, 0x1B, 0x13, 0x78, 0x00, 0x00, 0x00, 0x00, 0x03, 0xE8, 0x00, 0x00, 0x22, 0xC7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x19, 0x00, 0x03, 0x0C, 0x02, 0x0B, 0x64, 0x0B, 0x5F, 0xFC, 0x83, 0x77};
+    // DD A5 00 18 10 39 10 3A 10 38 10 3A 10 3C 10 39 10 37 10 39 10 3B 10 3F 10 36 10 3A FC 74 77
+    uint8_t cellInfoResponse[] = {0xDD, 0xA5, 0x00, 0x18, 0x10, 0x39, 0x10, 0x3A, 0x10, 0x38, 0x10, 0x3A, 0x10, 0x3C, 0x10, 0x39, 0x10, 0x37, 0x10, 0x39, 0x10, 0x3B, 0x10, 0x3F, 0x10, 0x36, 0x10, 0x3A, 0xFC, 0x74, 0x77};
+    // DD A5 00 14 4C 48 2D 53 50 31 35 53 30 30 31 2D 50 31 33 53 2D 33 30 41 FB 39 77
+    uint8_t deviceInfoResponse[] = {0xDD, 0xA5, 0x00, 0x14, 0x4C, 0x48, 0x2D, 0x53, 0x50, 0x31, 0x35, 0x53, 0x30, 0x30, 0x31, 0x2D, 0x50, 0x31, 0x33, 0x53, 0x2D, 0x33, 0x30, 0x41, 0xFB, 0x39, 0x77};
     // std::string rxValue = pCharacteristic->getValue();
     auto rxValue = pCharacteristic->getData();
     auto len = pCharacteristic->getLength();
@@ -73,34 +94,46 @@ class MyRXCallbacks : public BLECharacteristicCallbacks
       Serial.println("*********");
     }
 
-    // if (memcmp(rxValue, basicInfoRequest, len) == 0)
-    // {
-    //   Serial.println("basic info request");
-    //   // {DD A5 00 1B 13 78 00 00 00 00 03 E8 00 00 22 C7 00 00 00 00 00 00 19 00 03 0C 02 0B 64 0B 5F FC 83 77};
-    //   uint8_t basicInfoResponse[] = {0xDD, 0xA5, 0x00, 0x1B, 0x13, 0x78, 0x00, 0x00, 0x00, 0x00, 0x03, 0xE8, 0x00, 0x00, 0x22, 0xC7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x19, 0x00, 0x03, 0x0C, 0x02, 0x0B, 0x64, 0x0B, 0x5F, 0xFC, 0x83, 0x77};
-    //   pTxCharacteristic->setValue(basicInfoResponse, sizeof(basicInfoResponse));
-    //   pTxCharacteristic->notify();
-    // }
-    // else if (memcmp(rxValue, cellInfoRequest, len) == 0)
-    // {
-    //   Serial.println("cell info request");
-    //   // DD A5 00 18 10 39 10 3A 10 38 10 3A 10 3C 10 39 10 37 10 39 10 3B 10 3F 10 36 10 3A FC 74 77
-    //   uint8_t cellInfoResponse[] = {0xDD, 0xA5, 0x00, 0x18, 0x10, 0x39, 0x10, 0x3A, 0x10, 0x38, 0x10, 0x3A, 0x10, 0x3C, 0x10, 0x39, 0x10, 0x37, 0x10, 0x39, 0x10, 0x3B, 0x10, 0x3F, 0x10, 0x36, 0x10, 0x3A, 0xFC, 0x74, 0x77};
-    //   pTxCharacteristic->setValue(cellInfoResponse, sizeof(cellInfoResponse));
-    //   pTxCharacteristic->notify();
-    // }
-    // else if (memcmp(rxValue, deviceInfoRequest, len) == 0)
-    // {
-    //   Serial.println("device info request");
-    //   // DD A5 00 14 4C 48 2D 53 50 31 35 53 30 30 31 2D 50 31 33 53 2D 33 30 41 FB 39 77
-    //   uint8_t deviceInfoResponse[] = {0xDD, 0xA5, 0x00, 0x14, 0x4C, 0x48, 0x2D, 0x53, 0x50, 0x31, 0x35, 0x53, 0x30, 0x30, 0x31, 0x2D, 0x50, 0x31, 0x33, 0x53, 0x2D, 0x33, 0x30, 0x41, 0xFB, 0x39, 0x77};
-    //   pTxCharacteristic->setValue(deviceInfoResponse, sizeof(deviceInfoResponse));
-    //   pTxCharacteristic->notify();
-    // }
-    // else
-    // {
-    //   Serial.println("unknown request");
-    // }
+    BLECharacteristic *txChar = pServer->getServiceByUUID(serviceUUID)->getCharacteristic(charUUID_tx);
+    if (txChar == NULL)
+    {
+      Serial.println("txChar not found");
+      return;
+    }
+
+    if (memcmp(rxValue, basicInfoRequest, len) == 0)
+    {
+      Serial.println("basic info request");
+
+      txChar->setValue(&basicInfoResponse[0], sizeof(basicInfoResponse));
+      txChar->notify();
+    }
+    else if (memcmp(rxValue, cellInfoRequest, len) == 0)
+    {
+      Serial.println("cell info request");
+      txChar->setValue(&cellInfoResponse[0], sizeof(cellInfoResponse));
+      txChar->notify();
+    }
+    else if (memcmp(rxValue, deviceInfoRequest, len) == 0)
+    {
+      Serial.println("device info request");
+      txChar->setValue(&deviceInfoResponse[0], sizeof(deviceInfoResponse));
+      txChar->notify();
+    }
+    else if (memcmp(rxValue, testRequest, len) == 0)
+    {
+      // pCharacteristic->setValue(rxValue, len);
+      // pCharacteristic->notify();
+      txChar->setValue(&basicInfoResponse[0], sizeof(basicInfoResponse));
+      txChar->notify();
+    }
+
+    else
+    {
+      txChar->setValue(rxValue, len);
+      txChar->notify();
+      Serial.println("unknown request");
+    }
   }
 };
 
@@ -128,20 +161,7 @@ class MyTXCallbacks : public BLECharacteristicCallbacks
     }
   }
 };
-// 0000ff01-0000-1000-8000-00805f9b34fb
-// NOTIFY, READ
-// Notifications from this characteristic is received data from BMS
 
-// 0000ff02-0000-1000-8000-00805f9b34fb
-// Write this characteristic to send data to BMS
-// READ, WRITE, WRITE NO RESPONSE
-static BLEUUID serviceUUID("0000ff00-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
-static BLEUUID charUUID_tx("0000ff01-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
-static BLEUUID charUUID_rx("0000ff02-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
-
-static BLEUUID faServiceUUID("0000fa01-0000-1000-8000-00805f9b34fb"); // xiaoxiang bms original module
-
-BLECharacteristic *m_pnpCharacteristic; // 0x2a50
 void setup()
 {
   Serial.begin(115200);
@@ -153,7 +173,7 @@ void setup()
   // Serial.println(charRxUUID.toString().c_str());
   // Serial.println(charTxUUID.toString().c_str());
   // Create the BLE Device
-  BLEDevice::init("BMS Clone");
+  BLEDevice::init("BMSClone");
   // Create the BLE Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
@@ -175,22 +195,22 @@ void setup()
   BLEDescriptor *p2901Descriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901)); // Characteristic User Description
   p2901Descriptor->setValue("xiaoxiang bms");
 
-// 0000ff01-0000-1000-8000-00805f9b34fb
-// NOTIFY, READ
-// Notifications from this characteristic is received data from BMS
- BLECharacteristic *pTxCharacteristic = pService->createCharacteristic(
+  // 0000ff01-0000-1000-8000-00805f9b34fb
+  // NOTIFY, READ
+  // Notifications from this characteristic is received data from BMS
+  pTxCharacteristic = pService->createCharacteristic(
       BLEUUID((uint16_t)0xff01),
       BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
   pTxCharacteristic->setCallbacks(new MyTXCallbacks());
   pTxCharacteristic->addDescriptor(new BLE2902());
   pTxCharacteristic->addDescriptor(p2901Descriptor);
 
-// 0000ff02-0000-1000-8000-00805f9b34fb
-// Write this characteristic to send data to BMS
-// READ, WRITE, WRITE NO RESPONSE
+  // 0000ff02-0000-1000-8000-00805f9b34fb
+  // Write this characteristic to send data to BMS
+  // READ, WRITE, WRITE NO RESPONSE
   BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
       BLEUUID((uint16_t)0xff02),
-      BLECharacteristic::PROPERTY_READ| BLECharacteristic::PROPERTY_WRITE| BLECharacteristic::PROPERTY_WRITE_NR);
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_WRITE_NR);
   pRxCharacteristic->setCallbacks(new MyRXCallbacks());
   // pRxCharacteristic->addDescriptor(new BLE2902());
   pRxCharacteristic->addDescriptor(p2901Descriptor);
@@ -210,7 +230,8 @@ void setup()
   pFaService->start();
 
   // Start advertising
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+
   BLEAdvertisementData advertisementData;
   // char sNameData[] = {0x00, 0xff, 0x28, 0x50};
   // advertisementData.setShortName(std::string(sNameData, sizeof sNameData));
@@ -218,9 +239,16 @@ void setup()
   char manData[] = {0x6c, 0x97, 0xe6, 0x38, 0xc1, 0xa4};
   advertisementData.setManufacturerData(std::string(manData, sizeof manData));
   pAdvertising->setAdvertisementData(advertisementData);
-
-  pAdvertising->start();
+  // pAdvertising->setScanResponse(true);
+  // pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  // pAdvertising->setMinPreferred(0x12);
+  pAdvertising->addServiceUUID(serviceUUID);
+  BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
+
+  // pTxCharacteristic->setValue(basicInfoResponse, sizeof(basicInfoResponse));
+  // pTxCharacteristic->notify();
+  // Serial.println("basic info send");
 }
 
 void loop()
@@ -234,6 +262,29 @@ void loop()
     // pTxCharacteristic->notify();
     // txValue++;
     // delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+    // switch (sendInfo)
+    // {
+    // case 1:
+    //   Serial.println("basic info send");
+    //   // pTxCharacteristic->setValue(basicInfoResponse, sizeof(basicInfoResponse));
+    //   // pTxCharacteristic->notify();
+    //   sendInfo = 0;
+    //   break;
+
+    // case 2:
+    //   Serial.println("cell info send");
+    //   // pTxCharacteristic->setValue(cellInfoResponse, sizeof(cellInfoResponse));
+    //   // pTxCharacteristic->notify();
+    //   sendInfo = 0;
+    //   break;
+
+    // case 3:
+    //   Serial.println("device info send");
+    //   // pTxCharacteristic->setValue(deviceInfoResponse, sizeof(deviceInfoResponse));
+    //   // pTxCharacteristic->notify();
+    //   sendInfo = 0;
+    //   break;
+    // }
   }
 
   // disconnecting
